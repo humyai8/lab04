a711f9998ab8b8194f8514a9e791282cd684c91a
humyai8 <supachaimos40@gmail.com>
2018-01-23 09:24:00 +0700

    ggg

--- src/main/java/rmuti/lab04/TestQueue.java
+++ src/main/java/rmuti/lab04/TestQueue.java
@@ -2,4 +2,17 @@ package rmuti.lab04;
 
 public class TestQueue {
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayListQueue queue = new ArrayListQueue();
+		queue.enqueque("macos");
+		queue.enqueque("linux");
+		queue.enqueque("windows");
+		System.out.println(queue);
+		
+		String data = (String)queue.dequeue();
+		System.out.println(queue);
+
+	}
+
 }
